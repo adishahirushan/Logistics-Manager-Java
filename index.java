3d0c4b5ab9d15f07d9b2fe9cc96efcf5559b4e1c
adishahirushan <adishahirushan@gmail.com>
2025-10-23 21:06:01 +0530

    compleate that main function

--- index.java
+++ index.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        boolean repeater_1 = true;
+
         // 1st menu.... start from here....
+        while (repeater_1) {           
+        
         System.out.println(" ");
         System.out.println(".....main menu..... ");
         System.out.println("1. I'm customer");
@@ -11,8 +15,21 @@ public class index {
         System.out.println("enter choice number : ");
         int choice_no_1 = sc.nextInt(); 
 
+        if (choice_no_1 == 1) {
+            customer();
+        }else if (choice_no_1 == 2) {
+            employer();
+        }else{
+            System.out.println("invalid choice please double check and try anain");
+            repeater_1 = true;
+        }
+        }
 
-
-
+    }
+    public static void customer() {
+        System.out.println("costomer check");
+    }
+    public static void employer() {
+        System.out.println("employer check");
     }
 }

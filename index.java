9d02e971334eb14cd10ded52a62596c0ae86fc3c
adishahirushan <adishahirushan@gmail.com>
2025-10-23 20:36:06 +0530

    code for print 1st manu on index java file

--- index.java
+++ index.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
-        System.out.println("check");
+        Scanner sc = new Scanner(System.in);
+        
+        // 1st menu.... start from here....
+        System.out.println(" ");
+        System.out.println(".....main menu..... ");
+        System.out.println("1. I'm customer");
+        System.out.println("2. I'm employer");
+        System.out.println("enter choice number : ");
+        int choice_no_1 = sc.nextInt(); 
+
+
+
+
     }
 }

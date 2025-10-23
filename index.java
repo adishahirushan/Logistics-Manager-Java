848de5c1bf9cb27c18782f2f217b0d1055ef519a
adishahirushan <adishahirushan@gmail.com>
2025-10-23 21:27:43 +0530

    code employer menu (and also code empty methods call from employer menu) and make few changes onmain menu

--- index.java
+++ index.java
@@ -7,29 +7,81 @@ public class index {
 
         // 1st menu.... start from here....
         while (repeater_1) {           
-        
-        System.out.println(" ");
-        System.out.println(".....main menu..... ");
-        System.out.println("1. I'm customer");
-        System.out.println("2. I'm employer");
-        System.out.println("enter choice number : ");
-        int choice_no_1 = sc.nextInt(); 
+            System.out.println(" ");
+            System.out.println(".....main menu..... ");
+            System.out.println("1. I'm customer");
+            System.out.println("2. I'm employer");
+            System.out.println("3. exit");
+            System.out.println("enter choice number : ");
+            int choice_no_1 = sc.nextInt(); 
 
-        if (choice_no_1 == 1) {
-            customer();
-        }else if (choice_no_1 == 2) {
-            employer();
-        }else{
-            System.out.println("invalid choice please double check and try anain");
-            repeater_1 = true;
-        }
+            if (choice_no_1 == 1) {
+                customer();
+            }else if (choice_no_1 == 2) {
+                employer();
+            }else if (choice_no_1 == 3) {
+                repeater_1 = false;
+            }else{
+                System.out.println("invalid choice please double check and try anain");
+                repeater_1 = true;
+            }
         }
 
     }
+//............................this methods call from main menu.....................................
+
     public static void customer() {
         System.out.println("costomer check");
+        
     }
     public static void employer() {
-        System.out.println("employer check");
+
+        Scanner sc = new Scanner(System.in);
+        boolean repeater_2 = true;
+
+        while (repeater_2) {
+            System.out.println(" ");
+            System.out.println(".....employer menu..... ");
+            System.out.println("1. City Management");
+            System.out.println("2. Delivery Records");
+            System.out.println("3. Performance Reports");
+            System.out.println("4. exit from employer menu");
+            System.out.println("enter choice number : ");
+
+            int choice_no_2 = sc.nextInt();
+
+            if (choice_no_2 == 1) {
+                city_management();
+            }else if (choice_no_2 == 2) {
+                delivery_records();
+            }else if (choice_no_2 == 3) {
+                performance_reports();
+            }else if (choice_no_2 == 4) {
+                repeater_2 = false;
+            }else{
+                System.out.println("invalid choice please double check and try anain");
+            }    
+        }
     }
+
+//............................end of methods call by main manu.....................................
+
+//............................this methods call from employer menu.................................
+    public static void city_management() {
+        System.out.println("check");
+        
+    }
+    public static void delivery_records() {
+        System.out.println("check");
+        
+    }
+    public static void performance_reports() {
+        System.out.println("check");
+        
+    }
+
+
+
+//............................end of methods call by employer manu.................................
+
 }
